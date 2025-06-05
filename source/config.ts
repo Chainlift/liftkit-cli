8f42d57100b5a6894f48a971c97cb6bfbcaf71be
Jermeiah S <owner@arouzing.xyz>
2025-06-05 14:22:20 -0400

    chore: migrate config to ts file

---
+++ source/config.ts
@@ -0,0 +1,34 @@
+export interface TsConfigJson {
+	compilerOptions: {
+		paths: {
+			[key: string]: string[];
+		};
+	};
+}
+
+export interface Config {
+	repo: string;
+	templaterepo: string;
+	aliases: Record<string, string>;
+	tsconfigjson: TsConfigJson;
+}
+
+export const config: Config = {
+	repo: 'https://liftkit.pages.dev',
+	templaterepo:
+		'https://raw.githubusercontent.com/Chainlift/liftkit-template/refs/heads/main',
+	aliases: {
+		components: '@/components',
+		utils: '@/lib/utils',
+		ui: '@/components/ui',
+		lib: '@/lib',
+		hooks: '@/hooks',
+	},
+	tsconfigjson: {
+		compilerOptions: {
+			paths: {
+				'@/*': ['./src/*'],
+			},
+		},
+	},
+};
